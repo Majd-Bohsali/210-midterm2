a4d01c102610d85fb6e55255733f14fa256e747f
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-19 13:29:42 -0700

    writting probabilies

--- 210-midterm-2-starter.cpp
+++ 210-midterm-2-starter.cpp
@@ -265,17 +265,22 @@ int main() {
                 int probVIP = rand() % 100 + 1; 
 
                 if (probServed <= 40) { // person at front is served
-                    
+                    cout << "\t" << list->getName(1) << " is served" << endl; 
+                    list->pop_front(); 
                 } else if (probJoin <= 60) {
-
+                    getline(inputFile, person);
+                    cout << "\t" << person << " joined the line" << endl; 
+                    list->push_back(person); 
                 } else if (probLeaveEnd <= 20) {
-                    
+                    cout << "(at the rear) left the line" << endl; 
                 } else if (probLeaveAny <= 10) {
                     
                 } else if (probVIP <= 10) {
                     
                 }
 
+                cout << "\tResulting line: "; 
+                list->print();
             }
         }
 
